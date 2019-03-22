89774a565dc4d615b59ed3a1739da6b2c0a35682
steinsun <48515037+steinsun@users.noreply.github.com>
2019-03-22 11:30:34 +0800

    Add files via upload

---
+++ audioset-downloader.py
@@ -0,0 +1,52 @@
+import pafy, os, shutil, time, ffmpy
+import pandas as pd
+import soundfile as sf 
+
+os.chdir("D:\downloader\download_audioset")
+
+file = pd.read_excel('balanced_train_segments.xlsx')
+
+audio_id = file.iloc[:,0].tolist()[2:]
+audio_start = file.iloc[:,1].tolist()[2:]
+audio_end = file.iloc[:,2].tolist()[2:]
+audio_labels = file.iloc[:,3].tolist()[2:]
+
+com_link='https://www.youtube.com/watch?v='
+
+for i in range(len(audio_id)):
+    dl_link = com_link + audio_id[i]
+    start = audio_start[i]
+    end = audio_end[i]
+    
+    try:
+        video = pafy.new(dl_link)
+        bestaudio = video.getbestaudio()
+        filename = bestaudio.download()
+        extension = bestaudio.extension
+
+        if extension not in ['.wav']:
+            xindex = filename.find(extension)
+            filename = filename[0:xindex]
+            ff = ffmpy.FFmpeg(
+                inputs = {filename + extension: None},
+                outputs = {filename + '.wav': None}
+                )
+            ff.run()
+            os.remove(filename+extension)
+
+        file = filename + '.wav'
+        data, samplerate = sf.read(file)
+        totalframes = len(data)
+        totalseconds = totalframes/samplerate
+        startsec = start
+        startframe = samplerate*startsec
+        endsec = end
+        endframe = samplerate*endsec
+        sf.write(file, data[startframe:endframe], samplerate)
+
+        os.remove(file)
+
+    except:
+        print('no urls')
+    
+    
